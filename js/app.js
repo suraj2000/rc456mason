1ce2de756eba54463c946e4a85ac26e90f904f82
shivanyshenoy <shivany.shenoy@panerabread.com>
2014-06-08 19:45:46 -0500

    updated app highlights - turned the outline into a collapsible component.

--- js/app.js
+++ js/app.js
@@ -34,28 +34,19 @@ app.controller('DashboardCtrl', ['$scope', function DashboardCtrl($scope) {
 
 app.controller('HighlightsCtrl', ['$scope', function HighlightsCtrl($scope) {
 
-    $scope.descriptions = [
-        "Users can select any grade for the concrete mix (M15, M20, M25, M30, M35 or M40) for the above element along with steel grade (MS or High strength steel).",
-        "Each of the above element has two common data such as clear cover and main reinforcement steel bar diameter. App uses recommended data for clear cover as per IS 456 : 2000 that can be edited depending upon severity of the application. Similarly, one can edit bar diameter during analysis in order to satisfy percent steel and minimum number of bars that is recommended as per IS 456 : 2000. In addition each element has its own default dimensions that can be edited to suit the given application. Thus one can quickly build any application for onsite analysis as shown in screen shots.",
-        "Analysis is done based on actual reinforcement steel bars provided for the given section and load. Report has reference to different applicable clauses as per IS 456 : 2000 for quick reference. Some of the key figures and tables are also provided for instant verification of the design data.",
-        "Report summary includes total concrete volume, quantity of cement and steel required for the job that would help in material requirement planning to complete the casting job.",
-        "A RCC structure may have many different elements such as footing, column, beam, slab etc. Therefore each structure can be saved with unique title where one can add as many elements to this structure with reference name or code number for identification. A file summary is also provided that gives total structure summary along with individual element material summary.",
-        "Users can also create many different files for the same structure for design and cost optimization.",
-        "App provides a short cheklist for each element to facilitate on-site data collection that helps in the analysis. In addition, App notes gives brief description of different terminology used with design data and refernce to IS 456 : 2000.",
-        "This app can be a very helpful tool for engineers and contractors to evaluate any rcc structure element before casting to ensure full compliance under IS 456 : 2000 code for its integrity and fail safe design under given load conditions."
-    ];
-
     $scope.rccStructureElements = [
         {
             "name": "Slab",
             "subNav": ["Simply Supported (Wall-rest)",
                 "Cantilever",
                 "Continuous"
-            ]
+            ],
+            "content":  "Users can select any grade for the concrete mix (M15, M20, M25, M30, M35 or M40) for the above element along with steel grade (MS or High strength steel)."
         },
         {
             "name": "Beam",
-            "subNav": []
+            "subNav": [],
+            "content" : "Each of the above element has two common data such as clear cover and main reinforcement steel bar diameter. App uses recommended data for clear cover as per IS 456 : 2000 that can be edited depending upon severity of the application. Similarly, one can edit bar diameter during analysis in order to satisfy percent steel and minimum number of bars that is recommended as per IS 456 : 2000. In addition each element has its own default dimensions that can be edited to suit the given application. Thus one can quickly build any application for onsite analysis as shown in screen shots."
         },
         {
             "name": "Flanged Beam",
@@ -63,28 +54,34 @@ app.controller('HighlightsCtrl', ['$scope', function HighlightsCtrl($scope) {
                 "T beam",
                 "L-Beam",
                 "Isolated"
-            ]
+            ],
+            "content" : "Analysis is done based on actual reinforcement steel bars provided for the given section and load. Report has reference to different applicable clauses as per IS 456 : 2000 for quick reference. Some of the key figures and tables are also provided for instant verification of the design data."
         },
         {
             "name": "Column (Square/Rectangular)",
-            "subNav": []
+            "subNav": [],
+            "content":  "Report summary includes total concrete volume, quantity of cement and steel required for the job that would help in material requirement planning to complete the casting job."
         },
         {
             "name": "Round Column",
-            "subNav": []
+            "subNav": [],
+            "content":"A RCC structure may have many different elements such as footing, column, beam, slab etc. Therefore each structure can be saved with unique title where one can add as many elements to this structure with reference name or code number for identification. A file summary is also provided that gives total structure summary along with individual element material summary."
 
         },
         {
             "name": "Footing",
-            "subNav": []
+            "subNav": [],
+            "content":   "Users can also create many different files for the same structure for design and cost optimization."
         },
         {
             "name": "Staircase-A (on Slab)",
-            "subNav": []
+            "subNav": [],
+            "content": "App provides a short cheklist for each element to facilitate on-site data collection that helps in the analysis. In addition, App notes gives brief description of different terminology used with design data and refernce to IS 456 : 2000."
         },
         {
             "name": "Staircase-B (on Slab)",
-            "subNav": []
+            "subNav": [],
+            "content":  "This app can be a very helpful tool for engineers and contractors to evaluate any rcc structure element before casting to ensure full compliance under IS 456 : 2000 code for its integrity and fail safe design under given load conditions."
         }
     ];
 
